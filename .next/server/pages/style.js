e9518ee73cea858647eab0171fc356788e6b9872
JoaoMarcelo22 <joao_marcelo.22@hotmail.com>
2023-01-11 02:48:54 -0300

    feat: :bug: build

---
+++ .next/server/pages/style.js
@@ -0,0 +1,40 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 472;
+exports.ids = [472];
+exports.modules = {
+
+/***/ 261:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "container": () => (/* binding */ container)
+});
+
+;// CONCATENATED MODULE: external "styled-components"
+const external_styled_components_namespaceObject = require("styled-components");
+var external_styled_components_default = /*#__PURE__*/__webpack_require__.n(external_styled_components_namespaceObject);
+;// CONCATENATED MODULE: ./pages/style.tsx
+
+const container = (external_styled_components_default()).div`
+`;
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(261));
+module.exports = __webpack_exports__;
+
+})();
